7ba1fcd6c1055ff637a70b21857847bec1fe675b
Steve Chiarelli <spchiare@yahoo.com>
2020-12-21 23:14:54 -0500

    Content removal

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -4,9 +4,9 @@ import {DataContext} from "../AppData";
 class Contact extends React.Component {
     static contextType = DataContext;
 
-    url = "https://cms.chiatek.com/users/api";
-    database = "spchiare_stevechiarelli";
-    user = "1002";
+    url = "";
+    database = "";
+    user = "";
 
     constructor() {
         super();
